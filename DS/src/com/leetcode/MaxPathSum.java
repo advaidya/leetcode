a243bfeff40bd09571f4fd3860d211b57c83475c
Adityanarayan vaidya <adityanarayan.vaidya@gmail.com>
2013-02-02 16:41:33 +0530

    Added any to any node max path sum calculation.

--- DS/src/com/leetcode/MaxPathSum.java
+++ DS/src/com/leetcode/MaxPathSum.java
@@ -13,6 +13,7 @@ MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVE
 FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 **/
+
 public class MaxPathSum {
 
 	public static class TreeNode {
@@ -22,82 +23,94 @@ public class MaxPathSum {
 	      public TreeNode(int x) { val = x; }
 	}
 	
-	 public int maxPathSum(TreeNode root) {
-				
-		 if (root!=null) {
-			 int mSum = root.val;
-			 
-			 if (root.left==null && root.right==null) {
-				 return mSum;
-			 } else if (root.left == null) {
-				 int rSum = maxpath(root.right);
-				 
-				 return Math.max(root.val+rSum,Math.max(rSum, root.val));
-			 } else if (root.right == null) {
-				 int lSum = maxpath(root.left);
-				 return Math.max(root.val+lSum,Math.max(lSum, root.val));
-			 } else {
-				 int lSum = maxpath(root.left);
-				 int rSum = maxpath(root.right);
-				 return Math.max(mSum+lSum+rSum,Math.max(mSum+rSum, Math.max(mSum+lSum, Math.max(mSum, Math.max(lSum, rSum)))));
-			 }
-		 } else {
-			 return -1;
-		 }		 
-	 }
-	 
-	 private int maxpath(TreeNode n) {
-		 if ( n == null) {
-			 return 0;
-		 } 
-		 
-		 if (n.left == null && n.right == null)
-			 return n.val;
-		 else if (n.left == null && n.right !=null) {
-			 return Math.max(n.val,n.val + maxpath(n.right));
-		 } else if (n.left != null && n.right == null) {
-			 return Math.max(n.val, n.val + maxpath(n.left));
-		 } else {
-			 return Math.max(n.val, n.val + Math.max(maxpath(n.left), maxpath(n.right)));
-		 }
-		 
-	 }
+	private int maxsum;
+
+	/**
+	 *  Given a binary tree, find the maximum path sum.
+
+		The path may start and end at any node in the tree.
 	
+		For example:
+		Given the below binary tree,
+
+	       	 1
+      		/ \
+	       2   3
+
+	 * 	@author Adityanarayan M Vaidya
+ 	 *
+ 	 */
+
+	public int maxPathSum(TreeNode root) {
+		
+		if (root!=null) {
+			maxsum=root.val;
+			mPathSum(root);
+		}
+		
+		return maxsum;
+	}
+	
+	private int mPathSum(TreeNode root) {
+		
+		if (root.right == null && root.left == null) {
+			return root.val;
+		} else if (root.right==null && root.left!=null) {
+			
+			int ls = mPathSum(root.left);
+			int tmpsum = Math.max(Math.max(root.val, ls),root.val+ls);
+			if (tmpsum>maxsum)
+				maxsum=tmpsum;
+			
+			return Math.max(root.val, root.val+ls);
+			
+		} else if (root.right!=null && root.left==null) {
+			
+			int rs = mPathSum(root.right);
+			int tmpsum = Math.max(Math.max(root.val, rs),root.val+rs);
+			if (tmpsum>maxsum)
+				maxsum=tmpsum;
+			
+			return Math.max(root.val, root.val+rs);
+			
+		} else {
+			
+			int rs = mPathSum(root.right);
+			int ls = mPathSum(root.left);
+			/*
+			 * Find the max path among left path, right path and 
+			 * current node.
+			 */
+			int tmpsum = Math.max(root.val, rs);
+			tmpsum = Math.max(tmpsum,ls);
+			tmpsum = Math.max(tmpsum, root.val+rs);
+			tmpsum = Math.max(tmpsum, root.val+ls);
+			tmpsum = Math.max(tmpsum, root.val+rs+ls);
+			
+			if (tmpsum>maxsum)
+				maxsum=tmpsum;
+			
+			return Math.max(Math.max(root.val, root.val+rs),root.val+ls);
+			
+		}
+	}
+		
 	public static void main(String[] args) {
 
-		/*TreeNode root = new TreeNode(10);
-		
-		root.left = new TreeNode(5);
-		root.right = new TreeNode(13);
-		
-		root.left.left = new TreeNode(1);
-		root.left.right = new TreeNode(7);
-		
-		root.left.right.left = new TreeNode(6);
-		root.left.right.right = new TreeNode(8);
-		
-		root.right = new TreeNode(13);
-		root.right.right = new TreeNode(16);
-		root.right.right.left = new TreeNode(14);*/
-		
-		//TreeNode root = new TreeNode(-2);
-		//root.right = new TreeNode(-1);
-		
 		TreeNode root = new TreeNode(1);
 		
-		root.right = new TreeNode(2);
+		root.right = new TreeNode(-3);
 		
-		root.right.right = new TreeNode(3);
+		root.left = new TreeNode(-2);
 		
-		root.right.right.right = new TreeNode(4);		
+		root.right.left = new TreeNode(-2);		
 		
-		root.right.right.right.right = new TreeNode(5);
+		root.left.left = new TreeNode(1);
+		root.left.right = new TreeNode(3);
 		
+		root.left.left.left = new TreeNode(-1);
 		MaxPathSum mps = new MaxPathSum();
-		System.out.println(mps.maxPathSum(root));
-		
-		//root.left = null;
-		//System.out.println(mps.maxPathSum(root));
+		System.out.println(mps.mPathSum(root));
 	}
 
 }
