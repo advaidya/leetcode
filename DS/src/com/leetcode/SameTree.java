3262dce53d60b984428acd09b912e7b2f50ca406
Adityanarayan vaidya <adityanarayan.vaidya@gmail.com>
2013-02-03 22:40:18 +0530

    Added recursive isSymmetric function.

--- DS/src/com/leetcode/SameTree.java
+++ DS/src/com/leetcode/SameTree.java
@@ -50,7 +50,54 @@ public class SameTree {
 		}
 		
 		return false;
-    }
+    	}
+
+	/*
+	 * Given a binary tree, check whether it is a mirror of itself 
+	 * (ie, symmetric around its center).
+
+	   For example, this binary tree is symmetric:
+
+    	 1
+   		/ \
+  	   2   2
+ 	  / \ / \
+	 3  4 4  3
+
+	But the following is not:
+
+    	1
+   	   / \
+  	  2   2
+   	   \   \
+   		3    3
+
+	Note:
+	Bonus points if you could solve it both recursively and iteratively. 
+	 */
+	public boolean isSymmetric(TreeNode root) {
+    	
+		if (root==null)
+			return true;
+		else if ((root.left==null&&root.right!=null) || (root.left!=null&&root.right==null))
+			return false;
+		else
+			return isSymmetric(root.left, root.right);
+    	}
+	
+	private boolean isSymmetric(TreeNode p, TreeNode q) {
+		if ((p==null&&q==null)) {
+    	   return true;   
+		} else if ((p==null&&q!=null)||(p!=null&&q==null)) {
+    	   return false;
+		} else  {
+			if (p.val==q.val) 
+                return isSymmetric(p.left, q.right) && isSymmetric(p.right, q.left);
+            else 
+                return false;
+		}
+	}
+
 	
 	public static void main(String[] args) {
 		
